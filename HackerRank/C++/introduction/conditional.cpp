8d339b7942c99bf027ed41bb9b60dfcbbafea877
jjin1115 <jjin1115@gmail.com>
2020-09-13 21:22:07 +0900

    practice for C++ in Hacker rank conditional statements

---
+++ HackerRank/C++/introduction/conditional.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    // Write Your Code Here
+    vector<string> output = {"Greater than 9", "one", "two", "three", "four", "five",
+                             "six", "seven", "eight", "nine"};
+
+    if(n > 9) {
+        cout << output[0] << endl;
+    } else {
+        cout << output[n] << endl;
+    }
+    return 0;
+}
