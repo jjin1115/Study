6ed98335bf2c227b6a9bddfe46560841d116ec8a
jjin1115 <jjin1115@gmail.com>
2020-10-06 22:15:40 +0900

    stringstream

---
+++ HackerRank/C++/introduction/stringstream.cpp
@@ -0,0 +1,35 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    // Complete this function
+    vector<int> num;
+    char ch;
+    int count = 0;
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] ==',') {
+            count++;
+        }
+    }
+    num.resize(count+1);
+    stringstream str1(str);
+    int i = 0;
+    for(; i <= count ; i++) {
+        str1 >> num[i] >> ch ;
+
+    }
+    return num;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+
+    return 0;
+}
