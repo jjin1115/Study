b375440f8d159e15947d9f0d1c632f0dac3e6542
jjin1115 <jjin1115@gmail.com>
2020-10-10 20:51:42 +0900

    Rectangle Area

--- HackerRank/C++/introduction/stringstream.cpp
+++ HackerRank/C++/introduction/stringstream.cpp
@@ -9,7 +9,7 @@ vector<int> parseInts(string str) {
     char ch;
     int count = 0;
     for(int i = 0; i < str.length(); i++){
-        if(str[i] ==',') {
+        if(str[i] == ',') {
             count++;
         }
     }
