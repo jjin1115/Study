556b0873f28d4c6d9744a3a2b05ce8bfe4950f10
jjin1115 <jjin1115@gmail.com>
2020-10-20 21:27:51 +0900

    AttributeParser

---
+++ HackerRank/C++/introduction/AttributeParser.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q;
+    cin >> n >> q;
+    vector<string> hrml;
+    vector<string> query;
+    hrml.resize(n);
+    query.resize(q);
+
+    for(int i = 0; i < n; i++) {
+        cin >> hrml[i] ;
+
+    }
+    return 0;
+}
