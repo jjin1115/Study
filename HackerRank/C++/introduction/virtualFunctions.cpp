d60e8c3f9f12545421d53e4fcad6523503f5532e
jjin1115 <jjin1115@gmail.com>
2020-10-21 21:51:24 +0900

    virtual functions

---
+++ HackerRank/C++/introduction/virtualFunctions.cpp
@@ -0,0 +1,88 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Person {
+public:
+
+    virtual void getdata() {}
+    virtual void putdata() {}
+protected:
+    string name;
+    int age;
+    int cur_id;
+
+};
+class Professor :public Person {
+public:
+    Professor() {
+        cur_id = id++;
+    }
+
+    void getdata(){
+        cin >> name >> age >> publications;
+    }
+    void putdata(){
+        cout << name << " " << age << " " << publications <<" " << cur_id << endl;
+    }
+private:
+    int publications;
+    static int id;
+};
+
+
+class Student : public Person {
+public:
+    Student(){
+        cur_id = id++;
+    }
+    void getdata(){
+        cin >> name >> age;
+        for(int i=0; i<6; i++) {
+            cin >> marks[i];
+        }
+    }
+    void putdata(){
+        int sum = 0;
+        for(int i=0; i<6; i++) {
+            sum += marks[i];
+        }
+        cout << name << " " << age<< " " << sum << " " <<cur_id << endl;
+
+    }
+private:
+    int marks[6];
+    static int id;
+};
+int Professor::id {1};
+int Student::id {1};
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
