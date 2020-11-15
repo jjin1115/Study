a8c581722cc39459a93a12fe4fb182d480228073
jjin1115 <jjin1115@gmail.com>
2020-11-15 21:34:04 +0900

    string arrange solve.

--- programmers/stringarrange.cpp
+++ programmers/stringarrange.cpp
@@ -2,35 +2,22 @@
 // Created by HJ on 2020-11-14.
 // https://programmers.co.kr/learn/courses/30/lessons/12915
 //
-
 #include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int sIndex;
 
-vector<string> solution(vector<string> strings, int n) {
-    vector<string> answer;
-
-    int count = 1;
-    answer.push_back(strings[0]);
-    int before = answer.size();
-    for(int i = 1; i < strings.size(); i++) {
-        for(int j = 0; j < answer.size(); j++) {
-            if(answer[j][n] > strings[i][n] ) {
-                answer.insert(answer.begin()+j, strings[i]);
-                break;
-            } else if (answer[j][n] == strings[i][n]) {
-                if(answer[j] > strings[i]) {
-                    answer.insert(answer.begin()+j, strings[i]);
-                } else {
-                    answer.push_back(strings[i]);
-                }
-            }
-        }
-        if(answer.size() <=i) {
-            answer.push_back(strings[i]);
-        }
+bool compare(string& str1, string& str2){
+    if (str1[sIndex] != str2[sIndex]) {
+        return str1[sIndex] < str2[sIndex];
+    } else {
+        return str1 < str2;
     }
-    return answer;
+}
+vector<string> solution(vector<string> strings, int n) {
+    sIndex = n;
+    sort(strings.begin(), strings.end(), compare);
+    return strings;
 }
