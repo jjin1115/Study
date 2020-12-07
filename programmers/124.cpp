06248eba5f7c1e4d37b696fb3c8ffefa786290d7
jjin1115 <jjin1115@gmail.com>
2020-12-07 22:36:59 +0900

    sum two num

--- programmers/124.cpp
+++ programmers/124.cpp
@@ -2,7 +2,6 @@
 // Created by HJ on 2020-12-06.
 //https://programmers.co.kr/learn/courses/30/lessons/12899#
 //
-
 #include <string>
 #include <vector>
 #include <iostream>
@@ -10,23 +9,26 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    vector<string> converter {"1","2","4"};
+    vector<string> converter {"4","1","2"};
     string temp = "";
     bool iscal = true;
 
+    // if(n <=3) {
+    //     iscal = false;
+    //     temp += to_string(n);
+    // }
     while (n>3) {
         cout << "n =  " << n <<endl;
         temp += to_string(n%3);
         n = n/3;
+
     }
-    if(iscal) {
-        temp += to_string(n/3);
-    }
+    temp += to_string(n-1);
 
     for(int i = temp.size()-1 ; i>= 0; i--) {
         int idx = temp[i]-'0';
         answer += converter[idx];
-        cout << temp[i] -'0'  << idx << " answer " <<answer <<endl;
+        cout << "i = " << temp[i] -'0'  << idx << " answer " <<answer <<endl;
     }
     return answer;
 }
