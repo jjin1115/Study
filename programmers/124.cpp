c3523cb1505a2227a0c5d26988898f07114d40ae
jjin1115 <jjin1115@gmail.com>
2020-12-08 22:25:53 +0900

    umm..

--- programmers/124.cpp
+++ programmers/124.cpp
@@ -2,6 +2,36 @@
 // Created by HJ on 2020-12-06.
 //https://programmers.co.kr/learn/courses/30/lessons/12899#
 //
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//using namespace std;
+//
+//string solution(int n) {
+//    string answer = "";
+//    vector<string> converter {"4","1","2"};
+//    string temp = "";
+//    bool iscal = true;
+//
+//    // if(n <=3) {
+//    //     iscal = false;
+//    //     temp += to_string(n);
+//    // }
+//    while (n>3) {
+//        cout << "n =  " << n <<endl;
+//        temp += to_string(n%3);
+//        n = n/3;
+//
+//    }
+//    temp += to_string(n-1);
+//
+//    for(int i = temp.size()-1 ; i>= 0; i--) {
+//        int idx = temp[i]-'0';
+//        answer += converter[idx];
+//        cout << "i = " << temp[i] -'0'  << idx << " answer " <<answer <<endl;
+//    }
+//    return answer;
+//}
 #include <string>
 #include <vector>
 #include <iostream>
@@ -9,26 +39,18 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    vector<string> converter {"4","1","2"};
-    string temp = "";
-    bool iscal = true;
-
-    // if(n <=3) {
-    //     iscal = false;
-    //     temp += to_string(n);
-    // }
-    while (n>3) {
-        cout << "n =  " << n <<endl;
-        temp += to_string(n%3);
+    string temp;
+    while (n>0) {
+        temp = to_string(n%3);
         n = n/3;
+        if(temp == "0") {
+            n = n-1;
+            temp = "4";
 
+        }
+        answer = temp + answer;
     }
-    temp += to_string(n-1);
 
-    for(int i = temp.size()-1 ; i>= 0; i--) {
-        int idx = temp[i]-'0';
-        answer += converter[idx];
-        cout << "i = " << temp[i] -'0'  << idx << " answer " <<answer <<endl;
-    }
+
     return answer;
 }
